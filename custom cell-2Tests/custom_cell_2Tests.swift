02ed57e0fddc02e5caa2c4483304dae6856a467e
emily <yuuamelie@gmail.com>
2018-01-18 10:34:54 -0800

    Initial Commit

---
+++ custom cell-2Tests/custom_cell_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  custom_cell_2Tests.swift
+//  custom cell-2Tests
+//
+//  Created by Emily on 1/18/18.
+//  Copyright © 2018 Emily. All rights reserved.
+//
+
+import XCTest
+@testable import custom_cell_2
+
+class custom_cell_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
